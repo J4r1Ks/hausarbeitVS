c9db85aa0af8539ccc36b7b085413b5cbac58b87
jstrelow <jarik.strelow@hs-osnabrueck.de>
2025-07-21 15:16:59 +0200

    added UserRepository

---
+++ src/main/java/de/hsos/vs/pong/auth/UserRepository.java
@@ -0,0 +1,15 @@
+package de.hsos.vs.pong.auth;
+
+import de.hsos.vs.pong.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/*
+ * Erklärung was JpaRepository macht: https://www.baeldung.com/the-persistence-layer-with-spring-data-jpa
+ *                                    https://www.geeksforgeeks.org/springboot/spring-boot-jparepository-with-example/
+ * Erklärung was Optional ist: https://docs.oracle.com/javase/8/docs/api/java/util/Optional.html
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
