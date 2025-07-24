0154aa40d1804d3e198562e0c248ab76e647925e
7Ratchet <109304910+7Ratchet@users.noreply.github.com>
2025-07-24 22:35:29 +0200

    Added crude player controls, still a lot to be desired

--- src/main/java/de/hsos/vs/pong/game/GameEngine.java
+++ src/main/java/de/hsos/vs/pong/game/GameEngine.java
@@ -2,19 +2,25 @@ package de.hsos.vs.pong.game;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.Arrays;
 
 public class GameEngine extends JFrame {
 
     private Color mainColor = new Color(20, 60, 100);
     private Color secondaryColor = new Color(100, 60, 20);
 
-    private Color player1 = new Color(100, 20, 20);
-    private Color player2 = new Color(20, 100, 20);
-    private Color player3 = new Color(100, 100, 20);
-    private Color player4 = new Color(100, 20, 100);
+    private Color player1Color = new Color(100, 20, 20);
+    private Color player2Color = new Color(20, 100, 20);
+    private Color player3Color = new Color(100, 100, 20);
+    private Color player4Color = new Color(100, 20, 100);
 
     private int playerWidth = 12;
     private int playerHeight = 120;
+
+    private PlayerController player1 = new PlayerController(true);
+    private int player1Velocity = 0;
+    private int player1Position = 300;
 
     public GameEngine() {
         this.setTitle("Quong");
@@ -31,25 +37,41 @@ public class GameEngine extends JFrame {
         label.setVisible(true);
         this.add(label);
 
+        this.addKeyListener(player1);
 
         this.pack();
         this.setVisible(true);
+    }
+
+    @Override
+    public void update(Graphics g) {
+        super.update(g);
+        //this.paint(this.getGraphics());
+        if(player1.upPressed){
+            player1Velocity = 1;
+            player1Position -= 20;
+        }else if(player1.downPressed){
+            player1Velocity = -1;
+            player1Position += 20;
+        }else{
+            player1Velocity = 0;
+        }
     }
 
     public void paint(Graphics g) {
         super.paint(g);
         g.fillRect(0, 0, 800, 800);
 
-        g.setColor(player1);
-        g.fillRect(playerWidth-5, 300, playerWidth, playerHeight);
+        g.setColor(player1Color);
+        g.fillRect(playerWidth-5, player1Position, playerWidth, playerHeight);
 
-        g.setColor(player2);
+        g.setColor(player2Color);
         g.fillRect(800-playerWidth, 300, playerWidth, playerHeight);
 
-        g.setColor(player3);
+        g.setColor(player3Color);
         g.fillRect(300, 30, playerHeight, playerWidth);
 
-        g.setColor(player4);
+        g.setColor(player4Color);
         g.fillRect(300, 800-19, playerHeight, playerWidth);
 
         g.setColor(Color.white);
@@ -58,7 +80,15 @@ public class GameEngine extends JFrame {
 
     public static void main(String[ ] args){
         GameEngine game = new GameEngine();
-        game.paint(game.getGraphics());
+        long timer = System.currentTimeMillis();
+
+        while(true){
+            if(System.currentTimeMillis()-timer > 200){
+                timer =  System.currentTimeMillis();
+                game.update(game.getGraphics());
+            }
+        }
+
     }
 }
 
--- src/main/java/de/hsos/vs/pong/game/PlayerController.java
+++ src/main/java/de/hsos/vs/pong/game/PlayerController.java
@@ -1,4 +1,58 @@
 package de.hsos.vs.pong.game;
 
-public class PlayerController {
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class PlayerController extends KeyAdapter {
+
+    private boolean vertical;
+    public boolean upPressed = false;
+    public boolean downPressed = false;
+    public boolean leftPressed = false;
+    public boolean rightPressed = false;
+
+    public PlayerController(boolean vertical) {
+        this.vertical = vertical;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        if(vertical){
+            if ((key == KeyEvent.VK_UP) && !upPressed) {
+                upPressed = true;
+            }
+            if ((key == KeyEvent.VK_DOWN) && !downPressed) {
+                downPressed = true;
+            }
+        }else{
+            if ((key == KeyEvent.VK_LEFT)  && !leftPressed) {
+                leftPressed = true;
+            }
+            if ((key == KeyEvent.VK_RIGHT)   && !rightPressed) {
+                rightPressed = true;
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if ((key == KeyEvent.VK_UP)) {
+            upPressed = false;
+        }
+        if ((key == KeyEvent.VK_DOWN)) {
+            downPressed = false;
+        }
+        if ((key == KeyEvent.VK_LEFT)) {
+            leftPressed = false;
+        }
+        if ((key == KeyEvent.VK_RIGHT)) {
+            rightPressed = false;
+        }
+    }
+
 }
