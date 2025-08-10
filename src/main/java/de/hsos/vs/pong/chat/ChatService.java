bd2dff1d15bf8d72f0513d91e3646bca14423e51
Jarik <jarik.strelow@hs-osnabrueck.de>
2025-08-10 15:20:33 +0200

    .

--- src/main/java/de/hsos/vs/pong/chat/ChatService.java
+++ src/main/java/de/hsos/vs/pong/chat/ChatService.java
@@ -1,4 +1,26 @@
 package de.hsos.vs.pong.chat;
 
+import de.hsos.vs.pong.model.ChatMessage;
+import org.springframework.stereotype.Service;
+/*
+@Service
 public class ChatService {
+    private final ChatRepository chatRepository;
+
+    public ChatService(ChatRepository chatRepository) {
+        this.chatRepository = chatRepository;
+    }
+
+    public ChatMessage sendLobbyMessage(String username, String message) {
+        if(message == null || message.trim().isEmpty() || message.length() > 500) {
+            throw new IllegalArgumentException("Message is invalid");
+        }
+
+        ChatMessage chatMessage = new ChatMessage(username, message, ChatMessage.ChatType.LOBBY);
+        return chatRepository.save(chatMessage);
+    }
+
+
 }
+
+ */
