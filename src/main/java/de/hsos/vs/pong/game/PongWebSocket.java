d001d6ac89fe854fc3850d92a40eae28a86510d9
7Ratchet <109304910+7Ratchet@users.noreply.github.com>
2025-08-10 17:23:44 +0200

    websocket ES FUNZT

--- src/main/java/de/hsos/vs/pong/game/PongWebSocket.java
+++ src/main/java/de/hsos/vs/pong/game/PongWebSocket.java
@@ -1,4 +1,34 @@
 package de.hsos.vs.pong.game;
 
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.PathParam;
+import jakarta.websocket.server.ServerEndpoint;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@ServerEndpoint(value = "/quong/{userID}")
 public class PongWebSocket {
+
+    private static final List<Session> sessions = new ArrayList<>();
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("userID") int userID) {
+        sessions.add(session);
+        //Ganz ganz wichtig, damit das Pong Fenster sich oeffnet
+        System.setProperty("java.awt.headless", "false");
+        if (!GraphicsEnvironment.isHeadless()) {
+            Game game = new Game();
+            game.game.choosePlayer = userID;
+            game.game.start(game.gameChat);
+        }
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+
+    }
 }
