4bd2c66a9e0bb3dbb424b0182115c3860c46b11e
7Ratchet <109304910+7Ratchet@users.noreply.github.com>
2025-08-11 21:45:25 +0200

    made player selection code more idiot proof

---
+++ src/main/java/de/hsos/vs/pong/game/GameDataPackage.java
@@ -0,0 +1,14 @@
+package de.hsos.vs.pong.game;
+
+public class GameDataPackage {
+    public float ballX;
+    public float ballY;
+    public float dirX;
+    public float dirY;
+    public float ballSpeed;
+
+    public int player1Pos;
+    public int player2Pos;
+    public int player3Pos;
+    public int player4Pos;
+}
